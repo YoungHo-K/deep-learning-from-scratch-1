27011783e429da4a2270899d75073d163a0f9702
Youngho Kim <130723311+YoungHo-K@users.noreply.github.com>
2024-06-18 21:58:46 +0900

    ADD: softmax with loss

--- common/layers.py
+++ common/layers.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from common.functions import softmax, cross_entropy_error
+
 
 class ReLU:
     def __init__(self):
@@ -39,12 +41,17 @@ class Affine:
     def __init__(self, W, b):
         self.W = W
         self.b = b
+
         self.x = None
+        self.origin_x_shape = None
         self.dW = None
         self.db = None
 
     def forward(self, x):
+        self.origin_x_shape = x.shape
+        x = x.reshape(x.shape[0], -1)
         self.x = x
+
         out = np.dot(x, self.W) + self.b
 
         return out
@@ -53,5 +60,33 @@ class Affine:
         dx = np.dot(dout, self.W.T)
         self.dW = np.dot(self.x.T, dout)
         self.db = np.sum(dout, axis=0)
+
+        dx = dx.reshape(*self.origin_x_shape)
+
+        return dx
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout = 1):
+        batch_size = self.t.shape[0]
+        if self.t.size == self.y.size:  # one-hot encoding
+            dx = (self.y - self.t) / batch_size
+
+        else:
+            dx = self.y.copy()
+            dx[np.arange(batch_size), self.t] -= 1
+            dx = dx / batch_size
 
         return dx
